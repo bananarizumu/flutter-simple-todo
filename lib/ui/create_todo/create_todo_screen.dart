cdacbc2ad1373221ed5c29378b26b3e8277190c6
井上直人 <inouenaoto0102@gmail.com>
2022-10-29 14:59:35 +0900

    create todo screen layout

--- lib/ui/create_todo/create_todo_screen.dart
+++ lib/ui/create_todo/create_todo_screen.dart
@@ -2,12 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../widgets/multi_line_text_field.dart';
+import '../widgets/single_line_text_field.dart';
+
 class CreateTodoScreen extends HookConsumerWidget {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _detailController = TextEditingController();
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    bool _isEnabled = false;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("TODO APP"),
+        title: Text('Todoの作成'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              SingleLineTextField(
+                Icons.title,
+                'タイトル',
+                'タイトルを入力してください',
+                _titleController,
+                validate: true,
+              ),
+              SizedBox(height: 4),
+              MultiLineTextField(
+                Icons.edit,
+                '詳細',
+                '詳細を入力してください',
+                _detailController,
+                validate: true,
+              ),
+              SizedBox(height: 4),
+              ElevatedButton(
+                child: const Text('Create'),
+                onPressed: !_isEnabled
+                    ? null
+                    : () {
+                        // 何かEnableの時の処理
+                      },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/todo_list/todo_list_screen.dart
+++ lib/ui/todo_list/todo_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_simple_todo/ui/todo_list/widget/todo_card.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../route/app_router.gr.dart';
@@ -19,6 +20,18 @@ class HomeScreen extends HookConsumerWidget {
             },
           )
         ],
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemBuilder: (context, n) {
+            if (n.isOdd) {
+              return Divider(color: Colors.blueGrey[300]);
+            }
+            return TodoCard();
+          },
+          // TODO
+          itemCount: 3,
+        ),
       ),
     );
   }
